988339d81d962170a53f7ce7ceace4e49f047161
canelian <psy1884@gamil.com>
2022-08-17 14:20:34 +0900

    update

--- js/footer.js
+++ js/footer.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
   //instagram hover
   $("#insta").hover(
-      function() {$(this).attr("src","img/instagram_2.png");
+      function() {$(this).attr("src","images/instagram_2.png");
       },
-      function() {$(this).attr("src","img/instagram.png"); 
+      function() {$(this).attr("src","images/instagram.png"); 
   });
   //youtube hover
   $("#youtube").hover(
-      function() {$(this).attr("src","img/youtube_2.png");
+      function() {$(this).attr("src","images/youtube_0.png");
       },
-      function() {$(this).attr("src","img/youtube.png"); 
+      function() {$(this).attr("src","images/youtube.png"); 
   });
   //kakao hover
   $("#kakaotalk").hover(
-      function() {$(this).attr("src","img/kakao_2.png");
+      function() {$(this).attr("src","images/kakao_0.png");
       },
-      function() {$(this).attr("src","img/kakao.png"); 
+      function() {$(this).attr("src","images/kakao.png"); 
   });
   //naver hover
   $("#naver").hover(
-      function() {$(this).attr("src","img/navrer_1.png");
+      function() {$(this).attr("src","images/navrer_1.png");
       },
-      function() {$(this).attr("src","img/navrer.png"); 
+      function() {$(this).attr("src","images/navrer.png"); 
   });
 });
